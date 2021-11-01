27959b85c32f1c528a801ddb4b6e84638a9502d1
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-10-31 21:37:55 -0400

    trying to get questions array

---
+++ js/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        "question": "First Question",
+        "answers": ["a","b","c","d"],
+        "correct index": 0
+    },
+    {
+        "question": "Second Question",
+        "answers": ["a","b","c","d"],
+        "correct index": 0
+    },
+    {
+        "question": "Third Question",
+        "answers": ["a","b","c","d"],
+        "correct index": 0
+    },
+]
--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+var currentQuestionIndex = 0;
 var startQuiz = document.querySelector("#start-quiz-button");
 var openingPage = document.querySelector("#main-opening-page");
 
-// startQuiz.addEventListener("click",openingPage.style.display)
+startQuiz.onclick = function createCurrentQuestion(){
+    if(currentQuestionIndex < questions.length) {
+        console.log(questions[currentQuestionIndex])
+    }
+};
 
 startQuiz.onclick = function() {
     if (openingPage.style.display !== 'none') {
@@ -9,5 +14,32 @@ startQuiz.onclick = function() {
     }
     else {
         openingPage.style.display = 'block';
-    }
+    };
+
 };
+
+// console.log(questions);
+
+// var question1 = document.querySelector("#question-1-page");
+// question1.style.display = "none";
+
+// startQuiz.addEventListener("click",openingPage.style.display)
+
+// startQuiz.onclick = function() {
+//     if (question1.style.display !== 'none') {
+//         question1.style.display = 'block';
+//     }
+//     else {
+//         question1.style.display = 'none';
+//     };
+// };
+
+
+//     function (){
+// if(openingPage.style.display == 'none'){
+//     question1.style.display == "block";
+// }
+// else{
+//     question1.style.display == "none";
+// }
+// };
