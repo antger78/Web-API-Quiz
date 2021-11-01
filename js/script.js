cec7eda344e11abff91598b48991e1bc34c2645e
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-10-31 23:47:42 -0400

    added a mess of files, added the hide main function and the log question into one function, making them both work on the same event

--- js/script.js
+++ js/script.js
@@ -1,22 +1,57 @@
 var currentQuestionIndex = 0;
 var startQuiz = document.querySelector("#start-quiz-button");
 var openingPage = document.querySelector("#main-opening-page");
+var questions = [
+    {
+        "question": "First Question",
+        "answers": ["a","b","c","d"],
+        "correct index": 0
+    },
+    {
+        "question": "Second Question",
+        "answers": ["a","b","c","d"],
+        "correct index": 0
+    },
+    {
+        "question": "Third Question",
+        "answers": ["a","b","c","d"],
+        "correct index": 0
+    },
+];
 
 startQuiz.onclick = function createCurrentQuestion(){
     if(currentQuestionIndex < questions.length) {
-        console.log(questions[currentQuestionIndex])
-    }
-};
-
-startQuiz.onclick = function() {
+        console.log(questions[currentQuestionIndex].question)
+    };
     if (openingPage.style.display !== 'none') {
         openingPage.style.display = 'none';
     }
     else {
         openingPage.style.display = 'block';
     };
-
 };
+
+// startQuiz.onclick = function() {
+//     if (openingPage.style.display !== 'none') {
+//         openingPage.style.display = 'none';
+//     }
+//     else {
+//         openingPage.style.display = 'block';
+//     };
+
+// };
+
+// var count = 60, timer = setInterval(function() {
+//     $("#60sec").html(count--);
+//     if(count == 1) clearInterval(timer);
+// }, 1000);
+
+// setInterval(myTimer, 1000);
+
+// function myTimer() {
+//   const d = new Date();
+//   document.getElementById("60sec").innerHTML = d.toLocaleTimeString();
+// }
 
 // console.log(questions);
 
@@ -43,3 +78,17 @@ startQuiz.onclick = function() {
 //     question1.style.display == "none";
 // }
 // };
+
+// function startCountdown(seconds) {
+//     let counter = seconds;
+      
+//     const interval = setInterval(() => {
+//       console.log(counter);
+//       counter--;
+        
+//       if (counter < 0 ) {
+//         clearInterval(interval);
+//         console.log('Ding!');
+//       }
+//     }, 1000);
+//   };
---
+++ js/timer.js
@@ -0,0 +1,13 @@
+function startCountdown(seconds) {
+    let counter = seconds;
+      
+    const interval = setInterval(() => {
+      console.log(counter);
+      counter--;
+        
+      if (counter < 0 ) {
+        clearInterval(interval);
+        console.log('Ding!');
+      }
+    }, 1000);
+  };
